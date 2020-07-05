79fe5bc55339d7e3dc52c946d37c025dae128321
salasming <salasming@126.com>
2020-07-05 23:33:38 +0800

    7th week homework

---
+++ Week_07/week07/src/TrieNode.java
@@ -0,0 +1,11 @@
+
+class TrieNode {
+    public char val;
+    public boolean isWord;
+    public TrieNode[] children = new TrieNode[26];
+    public TrieNode() {}
+    TrieNode(char c){
+        TrieNode node = new TrieNode();
+        node.val = c;
+    }
+}
